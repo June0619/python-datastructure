1b45fbc76a5f2829dced5c5cb873626f6faa0533
June0619 <jjw5050@hotmail.com>
2022-01-18 14:28:19 +0900

    이중 연결 리스트 인덱스 접근, 아이템 조회 추가, append 함수 수정

--- my_collections/doubly_circular_linked_list.py
+++ my_collections/doubly_circular_linked_list.py
@@ -50,7 +50,7 @@ class DoublyCircularLinkedList:
 
         # 노드의 링크 부분이 헤드로 돌아가지 않을때까지 반복
         while last_link.next is not None \
-                and last_link.next is not self.__head.next:
+                and last_link.next is not self.__head:
             last_link = last_link.next
 
         # data insert
@@ -62,7 +62,7 @@ class DoublyCircularLinkedList:
             new_node.prev = last_link
             self.__head.prev = new_node
             # next
-            new_node.next = self.__head.next
+            new_node.next = self.__head
         else:
             # prev
             new_node.prev = new_node
@@ -77,60 +77,19 @@ class DoublyCircularLinkedList:
         self.length += 1
         return self
 
-    def insert(self, index, data):
-        """
-        연결리스트의 특정 인덱스에 값을 삽입
-        :param index:
-        :param data:
-        :return self:
-        """
-        last_link = self.__head
-
-        for _ in range(index):
-            last_link = last_link.next
-
-        temp_node = last_link.next
-        last_link.next = Node(data)
-        last_link.next.next = temp_node
-
-        self.length += 1
-        return self
-
-    def delete(self, index):
-        """
-        연결리스트 특정 인덱스의 노드를 삭제한다
-        :param index:
-        :return:
-        """
-        last_link = self.__head
-
-        for _ in range(index):
-            last_link = last_link.next
-
-        # 삭제하려는 노드가 마지막이 아닌 경우
-        if last_link.next.next is not None:
-            temp = last_link.next.next
-            print("Deleted : ", last_link.next.data)
-            del last_link.next
-            last_link.next = temp
-        # 삭제하려는 노드가 마지막 노드인 경우
-        else:
-            del last_link.next
-            last_link.next = None
-
-        self.length -= 1
-
     def to_string(self):
         """
         연결리스트 전체 item 출력
         :return:
         """
-        last_link = self.__head.next
+        last_link = self.__head
 
         data_list = []
-        while last_link is not None:
+        while last_link is not None \
+                and last_link.next is not self.__head:
             data_list.append(str(last_link.data))
             last_link = last_link.next
+        data_list.append(str(last_link.data))
 
         print("Linked List Data [", ', '.join(data_list), "]")
 
@@ -139,11 +98,11 @@ class DoublyCircularLinkedList:
         인덱스 값을 통해 접근
         연결리스트 특성상 모든 노드를 순차조회 해야 한다.
         :param key:
-        :return last_link.data:
+        :return last_link:
         """
         last_link = self.__head
-        # 헤드의 더미데이터 제외
-        for _ in range(key+1):
+
+        for _ in range(key):
             last_link = last_link.next
         return last_link
 
--- test_package/dcLinkedList_test.py
+++ test_package/dcLinkedList_test.py
@@ -21,11 +21,17 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.dcLinkedList[0].prev, self.dcLinkedList[0])
 
     def test_append_twice(self):
-        # 연결리스트에 데이터 링크를 추가 테스트
+        # 데이터 여러개 추가 시 tail 과 head 연결 테스트
         self.dcLinkedList.append("TEST1")
         self.dcLinkedList.append("TEST2")
         self.assertEqual(self.dcLinkedList[0].next, self.dcLinkedList[1])
         self.assertEqual(self.dcLinkedList[1].prev, self.dcLinkedList[0])
+
+    def test_get_item(self):
+        self.dcLinkedList.append(1)
+        self.dcLinkedList.append(2)
+        self.dcLinkedList.append(3)
+        self.assertEqual(3, self.dcLinkedList[2].data)
 
     def tearDown(self):
         del self.dcLinkedList
